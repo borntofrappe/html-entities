40ae98226469063e74dc4fbee5eae458dab471dc
borntofrappe <borntofrappe@gmail.com>
2020-05-27 23:55:12 +0200

    add link to download static asset

---
+++ src/_data/site.js
@@ -0,0 +1,8 @@
+module.exports = {
+    url: 'http://html-entities-list.netlify.app/',
+    author: {
+        name: 'Gabriele Corti',
+        handle: 'borntofrappe',
+        twitter: 'https://twitter.com/borntofrappe',
+    }
+}
