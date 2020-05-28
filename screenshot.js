8dbbeb5ce967421cb9326477c778cc219f5f40af
borntofrappe <borntofrappe@gmail.com>
2020-05-28 18:51:42 +0200

    update file path

--- screenshot.js
+++ screenshot.js
@@ -3,19 +3,19 @@ const path = require('path');
 const fs = require('fs');
 
 // consider the file produced by 11ty in the dist folder
-const inputFile = './dist/index.html';
-const outputEntities = './dist/static/html-entities.png';
-const outputCard = './dist/static/twitter-card.png';
+const inputFile = 'dist/index.html';
+const outputEntities = 'dist/static/html-entities.png';
+const outputCard = 'dist/static/twitter-card.png';
 
-if(fs.existsSync(inputFile)) {
+if (fs.existsSync(inputFile)) {
   console.log(`Launching **puppeteer** for '${inputFile}'`);
-  const headless = true;
+  const headless = false;
   (async () => {
     const browser = await puppeteer.launch({ headless });
     const page = await browser.newPage();
-  
+
     await page.goto(path.resolve(inputFile), { waitUntil: 'networkidle0' });
-  
+
     const main = await page.$('main');
     const { x, y, width, height } = await main.boundingBox();
     await page.setViewport({
@@ -23,8 +23,11 @@ if(fs.existsSync(inputFile)) {
       height: height + y * 2,
       deviceScaleFactor: 1,
     });
-    console.log(`Writing '${outputEntities}' from '${inputFile}'`);
-    await page.screenshot({ path: outputEntities, clip: { x, y, width, height } });
+    console.log(`Writing ${outputEntities} from ${inputFile}`);
+    await page.screenshot({
+      path: outputEntities,
+      clip: { x, y, width, height },
+    });
 
     const dx = 30;
     const dy = 20;
@@ -33,7 +36,10 @@ if(fs.existsSync(inputFile)) {
     const w1 = width + dx * 2;
 
     console.log(`Writing '${outputCard}' from '${inputFile}'`);
-    await page.screenshot({ path: outputCard, clip: { x: x1, y: y1, width: w1, height: w1 / 2 } });
+    await page.screenshot({
+      path: outputCard,
+      clip: { x: x1, y: y1, width: w1, height: w1 / 2 },
+    });
     await browser.close();
   })();
 } else {
